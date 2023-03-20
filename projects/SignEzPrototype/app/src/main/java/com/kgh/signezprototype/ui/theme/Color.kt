88e2e1511793a1de5fb37b6abe86edb812732c47
오윤식 <skyland310@naver.com>
2023-03-20 10:56:42 +0900

    feat: color palette 세팅

--- projects/SignEzPrototype/app/src/main/java/com/kgh/signezprototype/ui/theme/Color.kt
+++ projects/SignEzPrototype/app/src/main/java/com/kgh/signezprototype/ui/theme/Color.kt
@@ -6,3 +6,14 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+//One UI Theme(Light)
+
+val OneBGWhite = Color(0xFFFFFFFF)
+val OneBGGrey = Color(0xFFF6F6F6)
+val OneBGDarkGrey = Color(0xFFE3E3E3)
+val OneBGBlue = Color(0xFF0381FE)
+
+val OneTextBlack = Color(0xFF000000)
+val OneTextGrey = Color(0xFF969696)
+val OneTextWhite = Color(0xFFFAFAFA)
