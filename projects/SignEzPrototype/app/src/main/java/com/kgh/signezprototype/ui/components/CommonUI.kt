19aefd208d48a32548cf5fb56cf8adfb367bbee7
오윤식 <skyland310@naver.com>
2023-03-21 16:25:03 +0900

    feat: implement abbbar button

--- projects/SignEzPrototype/app/src/main/java/com/kgh/signezprototype/ui/components/CommonUI.kt
+++ projects/SignEzPrototype/app/src/main/java/com/kgh/signezprototype/ui/components/CommonUI.kt
@@ -14,11 +14,12 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -281,7 +282,6 @@ fun TutorialStartButton(
         )
     }
 }
-
 
 @Preview
 @Composable
