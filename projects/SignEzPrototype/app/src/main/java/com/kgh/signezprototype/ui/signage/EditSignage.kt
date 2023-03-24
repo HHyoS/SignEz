e9553e6b43c8aea3624798777285fd9f6f03a7d6
부울경_1반_강기한 <rkdrlgks321@naver.com>
2023-03-24 10:18:00 +0900

    initialize error fix

--- projects/SignEzPrototype/app/src/main/java/com/kgh/signezprototype/ui/signage/EditSignage.kt
+++
@@ -1,8 +0,0 @@
-package com.kgh.signezprototype.ui.signage
-
-import com.kgh.signezprototype.ui.navigation.NavigationDestination
-
-object EditSignageScreenDestination : NavigationDestination {
-    override val route = "EditSignage"
-    override val titleRes = "Edit Signage"
-}
