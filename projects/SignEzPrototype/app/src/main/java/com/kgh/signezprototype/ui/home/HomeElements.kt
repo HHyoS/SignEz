7bf562afcd72abe1bbfbee43d3f7700df897c3f8
오윤식 <skyland310@naver.com>
2023-03-26 11:43:03 +0900

    feat: implemented pastresult component

--- projects/SignEzPrototype/app/src/main/java/com/kgh/signezprototype/ui/home/HomeElements.kt
+++ projects/SignEzPrototype/app/src/main/java/com/kgh/signezprototype/ui/home/HomeElements.kt
@@ -1,19 +1,25 @@
 package com.kgh.signezprototype.ui.home
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -22,14 +28,24 @@ import androidx.compose.ui.unit.dp
 import com.kgh.signezprototype.R
 import com.kgh.signezprototype.data.entities.Cabinet
 import com.kgh.signezprototype.data.entities.Signage
-import com.kgh.signezprototype.ui.components.WhiteButton
 import com.kgh.signezprototype.ui.components.FocusBlock
+import com.kgh.signezprototype.ui.components.WhiteButton
+import com.kgh.signezprototype.ui.theme.OneRippleGrey
 import com.kgh.signezprototype.ui.theme.SignEzPrototypeTheme
 
 @Composable //지난 분석 결과 틀
 fun PastResult(
     modifier: Modifier = Modifier,
 ) {
+    val itemList = listOf<Painter>(
+        painterResource(id = R.drawable.bluesign),
+        painterResource(id = R.drawable.bluesign),
+        painterResource(id = R.drawable.bluesign),
+        painterResource(id = R.drawable.bluesign),
+        painterResource(id = R.drawable.bluesign),
+        painterResource(id = R.drawable.bluesign),
+        painterResource(id = R.drawable.bluesign)
+    )
     Surface(
         modifier = modifier
             .background(androidx.compose.material.MaterialTheme.colors.surface)
@@ -38,7 +54,7 @@ fun PastResult(
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(start = 32.dp),
+                    .padding(start = 32.dp, top = 10.dp, bottom = 10.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
@@ -56,13 +72,36 @@ fun PastResult(
                         contentDescription = "지난 결과 보기",
                         tint = androidx.compose.material.MaterialTheme.colors.onSurface,
                         modifier = Modifier
-                            .height(21.dp)
-                            .width(21.dp)
+                            .height(23.dp)
+                            .width(23.dp)
                             .padding(end = 5.dp)
                     )
                 }
             } // Row 끝
-            Spacer(modifier = Modifier.padding(50.dp))
+
+            LazyRow(
+                contentPadding = PaddingValues(
+                    start = 24.dp,
+                    end = 24.dp
+                ),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(items = itemList) { item ->
+                    CompositionLocalProvider(LocalRippleTheme provides RippleCustomTheme) {
+                        Image(
+                            painter = item,
+                            contentDescription = "아이템",
+                            modifier = Modifier
+                                .width(100.dp)
+                                .height(100.dp)
+//                                .padding(5.dp)
+                                .clip(RoundedCornerShape(10.dp))
+                                .clickable { /* 해당 분석 결과로 가는 이벤트 */ }
+                        )
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
         } // Col 끝
     }
 }
@@ -165,4 +204,22 @@ fun VideoAnalysisBtn(navigateToVideo: () -> Unit) {
         onClickEvent = navigateToVideo
     )
 
+}
+
+private object RippleCustomTheme : RippleTheme {
+
+    //Your custom implementation...
+    @Composable
+    override fun defaultColor() =
+        RippleTheme.defaultRippleColor(
+            Color.Black,
+            lightTheme = true
+        )
+
+    @Composable
+    override fun rippleAlpha(): RippleAlpha =
+        RippleTheme.defaultRippleAlpha(
+            Color.Black,
+            lightTheme = true
+        )
 }
