9bdce9b90ece1b6d3ea2fd1f2f4ab742d81c3b15
gytkd33 <gytkd33@naver.com>
2023-03-29 15:48:10 +0900

    test:커밋테스트

--- SignageProblemShooting/app/src/main/java/com/signez/signageproblemshooting/service/PrePostProcessor.kt
+++ SignageProblemShooting/app/src/main/java/com/signez/signageproblemshooting/service/PrePostProcessor.kt
@@ -1,40 +1,53 @@
 package com.signez.signageproblemshooting.service
 
+
 import android.graphics.Rect
+import android.util.Log
+import java.util.*
 
-
+class Result(var classIndex: Int, var score: Float, var rect: Rect)
 object PrePostProcessor {
-    val NO_MEAN_RGB = floatArrayOf(0.0f, 0.0f, 0.0f)
-    val NO_STD_RGB = floatArrayOf(1.0f, 1.0f, 1.0f)
+    // for yolov5 model, no need to apply MEAN and STD
+    @JvmField
+    var NO_MEAN_RGB = floatArrayOf(0.0f, 0.0f, 0.0f)
+    @JvmField
+    var NO_STD_RGB = floatArrayOf(1.0f, 1.0f, 1.0f)
 
     // model input image size
-    const val mInputWidth = 640
-    const val mInputHeight = 640
+    @JvmField
+    var mInputWidth = 640
+    @JvmField
+    var mInputHeight = 640
 
     // model output is of size 25200*(num_of_class+5)
-    const val mOutputRow = 25200 // as decided by the YOLOv5 model for input image of size 640*640
-    const val mOutputColumn = 6 // left, top, right, bottom, score and class probability
-    const val mThreshold = 0.10f // score above which a detection is generated
-    const val mNmsLimit = 15
-
-    val signageClass: Array<String> = arrayOf("signage")
-    val errorModuleClass: Array<String> = arrayOf("error-module")
-
+    private const val mOutputRow =
+        25200 // as decided by the YOLOv5 model for input image of size 640*640
+    private const val mOutputColumn = 6 // left, top, right, bottom, score and 80 class probability
+    private const val mThreshold = 0.7f // score above which a detection is generated
+    private const val mNmsLimit = 15
+    lateinit var mClasses: Array<String>
     // The two methods nonMaxSuppression and IOU below are ported from https://github.com/hollance/YOLO-CoreML-MPSNNGraph/blob/master/Common/Helpers.swift
     /**
      * Removes bounding boxes that overlap too much with other boxes that have
      * a higher score.
-     *
-     * @param boxes an array of bounding boxes and their scores
-     * @param limit the maximum number of boxes that will be selected
-     * @param threshold used to decide whether boxes overlap too much
+     * - Parameters:
+     * - boxes: an array of bounding boxes and their scores
+     * - limit: the maximum number of boxes that will be selected
+     * - threshold: used to decide whether boxes overlap too much
      */
-    private fun nonMaxSuppression(boxes: ArrayList<DetectResult>, limit: Int, threshold: Float): ArrayList<DetectResult> {
-        // Do an argsort on the confidence scores, from high to low.
-        boxes.sortWith(Comparator { o1, o2 -> o1.score.compareTo(o2.score) })
+    fun nonMaxSuppression(
+        boxes: ArrayList<Result>,
+        limit: Int,
+        threshold: Float
+    ): ArrayList<Result> {
 
-        val selected = ArrayList<DetectResult>()
-        val active = BooleanArray(boxes.size) { true }
+        // Do an argsort on the confidence scores, from high to low.
+        Collections.sort(
+            boxes
+        ) { o1, o2 -> o1.score.compareTo(o2.score) }
+        val selected = ArrayList<Result>()
+        val active = BooleanArray(boxes.size)
+        Arrays.fill(active, true)
         var numActive = active.size
 
         // The algorithm is simple: Start with the box that has the highest score.
@@ -49,9 +62,7 @@ object PrePostProcessor {
                 val boxA = boxes[i]
                 selected.add(boxA)
                 if (selected.size >= limit) break
-
-                var j = i + 1
-                while (j < boxes.size) {
+                for (j in i + 1 until boxes.size) {
                     if (active[j]) {
                         val boxB = boxes[j]
                         if (IOU(boxA.rect, boxB.rect) > threshold) {
@@ -63,7 +74,6 @@ object PrePostProcessor {
                             }
                         }
                     }
-                    j++
                 }
             }
             i++
@@ -71,22 +81,30 @@ object PrePostProcessor {
         return selected
     }
 
-    private fun IOU(a: Rect, b: Rect): Float {
-        val areaA = (a.right - a.left) * (a.bottom - a.top)
+    /**
+     * Computes intersection-over-union overlap between two bounding boxes.
+     */
+    fun IOU(a: Rect, b: Rect): Float {
+        val areaA = ((a.right - a.left) * (a.bottom - a.top)).toFloat()
         if (areaA <= 0.0) return 0.0f
-
-        val areaB = (b.right - b.left) * (b.bottom - b.top)
+        val areaB = ((b.right - b.left) * (b.bottom - b.top)).toFloat()
         if (areaB <= 0.0) return 0.0f
-
-        val intersectionMinX = a.left.coerceAtLeast(b.left)
-        val intersectionMinY = a.top.coerceAtLeast(b.top)
-        val intersectionMaxX = a.right.coerceAtMost(b.right)
-        val intersectionMaxY = a.bottom.coerceAtMost(b.bottom)
-        val intersectionArea = (maxOf(intersectionMaxY - intersectionMinY, 0) *
-                maxOf(intersectionMaxX - intersectionMinX, 0)).toFloat()
+        val intersectionMinX = Math.max(a.left, b.left).toFloat()
+        val intersectionMinY = Math.max(a.top, b.top).toFloat()
+        val intersectionMaxX = Math.min(a.right, b.right).toFloat()
+        val intersectionMaxY = Math.min(a.bottom, b.bottom).toFloat()
+        val intersectionArea = Math.max(intersectionMaxY - intersectionMinY, 0f) *
+                Math.max(intersectionMaxX - intersectionMinX, 0f)
         return intersectionArea / (areaA + areaB - intersectionArea)
     }
 
+    fun getValue(a :Int): Int {
+        if(a < 0)
+            return 0
+        else
+            return a
+    }
+    @JvmStatic
     fun outputsToNMSPredictions(
         outputs: FloatArray,
         imgScaleX: Float,
@@ -95,20 +113,18 @@ object PrePostProcessor {
         ivScaleY: Float,
         startX: Float,
         startY: Float
-    ): ArrayList<DetectResult> {
-        val results = ArrayList<DetectResult>()
+    ): ArrayList<Result> {
+        val results = ArrayList<Result>()
         for (i in 0 until mOutputRow) {
             if (outputs[i * mOutputColumn + 4] > mThreshold) {
                 val x = outputs[i * mOutputColumn]
                 val y = outputs[i * mOutputColumn + 1]
                 val w = outputs[i * mOutputColumn + 2]
                 val h = outputs[i * mOutputColumn + 3]
-
                 val left = imgScaleX * (x - w / 2)
                 val top = imgScaleY * (y - h / 2)
                 val right = imgScaleX * (x + w / 2)
                 val bottom = imgScaleY * (y + h / 2)
-
                 var max = outputs[i * mOutputColumn + 5]
                 var cls = 0
                 for (j in 0 until mOutputColumn - 5) {
@@ -117,18 +133,26 @@ object PrePostProcessor {
                         cls = j
                     }
                 }
-
+                val centerX = (left + right) / 2
+                val centerY = (top + bottom) / 2
+                val width = right - left
+                val height = bottom - top
+                val newWidth = width * 1.4f
+                val newHeight = height * 1.4f
+                val newLeft = centerX - newWidth / 2
+                val newTop = centerY - newHeight / 2
+                val newRight = centerX + newWidth / 2
+                val newBottom = centerY + newHeight / 2
                 val rect = Rect(
-                    (startX + ivScaleX * left).toInt(),
-                    (startY + top * ivScaleY).toInt(),
-                    (startX + ivScaleX * right).toInt(),
-                    (startY + ivScaleY * bottom).toInt()
+                    getValue((startX + ivScaleX * newLeft).toInt()),
+                    getValue((startY + ivScaleY * newTop).toInt()),
+                    getValue((startX + ivScaleX * newRight).toInt()),
+                    getValue((startY + ivScaleY * newBottom).toInt())
                 )
-                val result = DetectResult(cls, outputs[i * mOutputColumn + 4], rect)
+                val result = Result(cls, outputs[i * mOutputColumn + 4], rect)
                 results.add(result)
             }
         }
         return nonMaxSuppression(results, mNmsLimit, mThreshold)
     }
-
 }
